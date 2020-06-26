f9eda24dd41c9b9abab3761e3df75502c68e749d
Nathan Long <nrlong13@gmail.com>
2020-06-26 12:48:58 -0400

    initial push.  api routes completed

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+if(process.env.NODE_env === "production"){
+    app.use(express.statis("client/build"));
+}
+
+app.use(routes)
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
+
+app.listen(PORT, () => {
+    console.log(`🌎 ===> API server now on port ${PORT}`)
+});

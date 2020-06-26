7f58417d96b535fbfc17986886c9eb6098f3c65f
Nathan Long <nrlong13@gmail.com>
2020-06-26 13:47:38 -0400

    updated components

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <div className="jumbotron text-center">
+            <div className="container text-light">
+                <h1></h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;

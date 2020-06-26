78848e92e87bce56788a64402c95db08490c2192
Nathan Long <nrlong13@gmail.com>
2020-06-26 14:26:21 -0400

    created pages for search and no results

---
+++ src/pages/Search.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Form from "../components/Form";
+import Results from "../components/Results";
+import API from "../utils/API";
+
+class Search extends React.Component {
+    state = {
+        value: "",
+        books: []
+    };
+
+    componentDidMount(){
+        this.searchBook()
+    }
+
+    makeBook = bookData => {
+        return{
+            _id: bookData.id,
+            title: bookData.volumeInfo.title,
+            authors: bookData.volumeInfo.description,
+            image: bookData.volumeInfo.imageLinks.thumbnail,
+            link: booksData.volumeInfo.previewLink
+        }
+    }
+
+    searchBook = query => {
+        API.getBook(query)
+            .then(res => this.setState({ books: res.data.items.map(bookData => this.makeBook(bookData)) }))
+            .catch(err => console.error(err));
+    };
+
+    handleInputChange = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = e => {
+        e.preventDefault();
+        this.searchBook(this.state.search);
+    };
+
+    render(){
+        return(
+
+            <div>
+                <Form
+                    search={this.state.search}
+                    handleInputChange={this.handleInputChange}
+                    handleFormSubmit={this.handleFormSubmit}
+                />
+                <div className="container">
+                    <h2>Results</h2>
+                    <Results books={this.state.books} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Search;
